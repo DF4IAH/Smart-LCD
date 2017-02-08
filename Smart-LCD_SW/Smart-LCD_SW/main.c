7b5da312d69443047f0d3485fc972e73f543646a
Ulrich Habel <espero7757@gmx.net>
2017-02-08 18:32:04 +0100

    SW - initial set-up with AVR-studio

---
+++ Smart-LCD_SW/Smart-LCD_SW/main.c
@@ -0,0 +1,18 @@
+/*
+ * Smart-LCD_SW.c
+ *
+ * Created: 08.02.2017 18:31:02
+ * Author : espero
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
