39152a4fc37c7345a8691d43a8236eff455e5814
Ulrich Habel <espero7757@gmx.net>
2017-04-03 23:07:53 +0200

    SW - some more TWI issues fixed

--- Smart-LCD_SW/Smart-LCD_SW/src/twi.h
+++ Smart-LCD_SW/Smart-LCD_SW/src/twi.h
@@ -10,16 +10,46 @@
 #define TWI_H_
 
 /* @see 10MHz-Ref-Osc application to compare the address of this "Smart-LCD" */
-#define TWI_SLAVE_ADDR		0x22
-//#define TWI_SLAVE_ADDR		0x20
+#define TWI_SLAVE_ADDR_10MHZREFOSC							0x22
+#define TWI_SLAVE_ADDR_SMARTLCD								0x30
 
 /* exactly the address above w/o masking out any bits */
-#define TWI_SLAVE_ADDR_BM   0b0000000
+#define TWI_SLAVE_ADDR_BM									0b0000000
 
-#define TWI_SLAVE_ADDR_GCE	0b1
+#define TWI_SLAVE_ADDR_GCE									0b1
+
+/* TWI TWSR states after mask is applied */
+#define TWI_TWSR_STATE_MASK									0xF8
+#define TWI_TWSR_START										0x08
+#define TWI_TWSR_REPEATEDSTART								0x10
+#define TWI_TWSR_M_SLAW_ADDR_ACK							0x18
+#define TWI_TWSR_M_SLAW_ADDR_NACK							0x20
+#define TWI_TWSR_M_SLAW_DATA_ACK							0x28
+#define TWI_TWSR_M_SLAW_DATA_NACK							0x30
+#define TWI_TWSR_M_SLAW_ARBIT_LOST							0x38
+#define TWI_TWSR_M_SLAR_ADDR_ACK							0x40
+#define TWI_TWSR_M_SLAR_ADDR_NACK							0x48
+#define TWI_TWSR_M_SLAR_DATA_ACK							0x50
+#define TWI_TWSR_M_SLAR_DATA_NACK							0x58
+
+#define TWI_TWSR_S_SLAW_MYADDR_RECEIVED						0x60
+#define TWI_TWSR_S_SLAW_MYADDR_ARBIT_LOST					0x68
+#define TWI_TWSR_S_SLAW_OMNIADDR_RECEIVED					0x70
+#define TWI_TWSR_S_SLAW_OMNIADDR_ARBIT_LOST					0x78
+#define TWI_TWSR_S_SLAW_MYADDR_DATA_ACK						0x80
+#define TWI_TWSR_S_SLAW_MYADDR_DATA_NACK					0x88
+#define TWI_TWSR_S_SLAW_OMNIADDR_DATA_ACK					0x90
+#define TWI_TWSR_S_SLAW_OMNIADDR_DATA_NACK					0x98
+#define TWI_TWSR_S_SLAW_STOP_REPEATEDSTART_RECEIVED			0xA0
+
+#define TWI_TWSR_S_SLAR_MYADDR_DATA_ACK						0xA8
+#define TWI_TWSR_S_SLAR_MYADDR_ARBIT_LOST					0xB0
+#define TWI_TWSR_S_SLAR_OMNIADDR_DATA_ACK					0xB8
+#define TWI_TWSR_S_SLAR_OMNIADDR_DATA_NACK					0xC0
+#define TWI_TWSR_S_SLAR_MYADDR_LASTDATA_ACK					0xC8
 
 
-// commands used by the 10MHz-Ref-Osc
+/* Commands used by the 10MHz-Ref-Osc */
 #define TWI_SMART_LCD_CMD_NOOP								0x00
 
 #define TWI_SMART_LCD_CMD_GETVER							0x01
